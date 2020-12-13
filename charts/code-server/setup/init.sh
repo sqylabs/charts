3db24d91beae657786f94353bd9d9c70f8ddf3ee
ghokun <gokhun@gmail.com>
2020-12-14 00:33:47 +0300

    Upgraded all charts
    Signed-off-by: ghokun <gokhun@gmail.com>

--- charts/code-server/setup/init.sh
+++ charts/code-server/setup/init.sh
@@ -37,11 +37,18 @@ fi
 echo "[ Installing extensions ]"
 while IFS=, read -r publisher extension version;
 do
-  download="https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$extension/$version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
-  echo "[ Downloading $publisher.$extension-$version.vsix ]"
-  wget $download -q -O $DOWNLOAD_DIR/$publisher.$extension-$version.vsix
-  echo "[ Installing $publisher.$extension-$version ]"
-  code-server --extensions-dir $EXTENSIONS_DIR --install-extension $DOWNLOAD_DIR/$publisher.$extension-$version.vsix
+EXTENSION_URL="https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$extension/$version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+INSTALLED_VERSION=$(code-server --extensions-dir /config/extensions --list-extensions --show-versions|grep -i $publisher|sed 's/.*\@//')
+  if [ "$INSTALLED_VERSION" == $version ]; then
+    echo "[ $publisher.$extension@$INSTALLED_VERSION.vsix is already installed ]"
+  else
+    echo "[ Downloading $publisher.$extension@$version.vsix ]"
+    #wget $EXTENSION_URL -q -O $DOWNLOAD_DIR/$publisher.$extension@$version.vsix
+    curl -s -o $DOWNLOAD_DIR/$publisher.$extension@$version.vsix $EXTENSION_URL
+    echo "[ Installing $publisher.$extension@$version ]"
+    code-server --extensions-dir $EXTENSIONS_DIR --install-extension $DOWNLOAD_DIR/$publisher.$extension@$version.vsix
+    rm $DOWNLOAD_DIR/$publisher.$extension@$version.vsix
+  fi
 done < <(grep "" $SETUP_DIR/extensions.csv)
 
 # Install oh-my-bash
